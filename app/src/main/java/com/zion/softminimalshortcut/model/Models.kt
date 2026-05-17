4086bf7955f484b8fc91ee1e468f0b5b5ae5c7c9
king0929zion <king0929zion@gmail.com>
2026-05-17 08:52:11 +0800

    Improve app picker model metadata

--- app/src/main/java/com/zion/softminimalshortcut/model/Models.kt
+++ app/src/main/java/com/zion/softminimalshortcut/model/Models.kt
@@ -3,7 +3,9 @@ package com.zion.softminimalshortcut.model
 data class InstalledApp(
     val label: String,
     val packageName: String,
-    val activityName: String
+    val activityName: String,
+    val searchKey: String,
+    val isSystemApp: Boolean
 )
 
 data class SavedShortcut(
